8c4aa3e14064a7a9ea27b5e43abb0556a5acb3bd
aiomran <angeleanta.io@gmail.com>
2023-05-25 22:28:50 -0500

    Delete form.php

--- form.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Client's Name <?php echo $_POST["client-name"]; ?><br>
-Company <?php echo $_POST["comp-name"]; ?><br>
-Email: <?php echo $_POST["email"]; ?>
-Message: <?php echo $_POST["message"]; ?>
-</body>
-</html>
