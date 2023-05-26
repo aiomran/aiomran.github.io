8016c03de4f6ac1b84600d4aa7e432a7a0b91011
aiomran <angeleanta.io@gmail.com>
2023-05-25 22:10:08 -0500

    Add files via upload

---
+++ form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Client's Name <?php echo $_POST["client-name"]; ?><br>
+Company <?php echo $_POST["comp-name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?>
+Message: <?php echo $_POST["message"]; ?>
+</body>
+</html>
